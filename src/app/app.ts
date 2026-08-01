dc7139dd251b29cb396ad1f5a0324e30dc010a18
Anshu <anshugupta29a@gmail.com>
2026-08-01 13:38:47 +0530

    Added routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MyComponent } from "../components/my-component/my-component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MyComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/components/my-component/my-component.ts
+++ src/components/my-component/my-component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-my-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './my-component.html',
   styleUrl: './my-component.css',
 })
 export class MyComponent {
-
-showMessage() {
-  alert('Hello from MyComponent!');
-}
+   username: string = 'Happy Coding!';
 }
